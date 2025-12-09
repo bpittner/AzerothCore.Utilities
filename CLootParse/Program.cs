af8fd7e307af5d5df1df34bf9744e22f836eaa01
Ben Pittner <bpittner@Bens-MacBook-Air.local>
2025-12-09 15:08:04 -0600

    Initial commit

---
+++ CLootParse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzerothCore.Utilities.CLootParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
